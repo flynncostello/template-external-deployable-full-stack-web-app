3ac466fbda7e0167c7ce4ccf0729e63ccf541fd1
Flynn Costello <flynn.p.costello@outlook.com>
2024-03-12 17:34:08 +1100

    deployable env var frontend fixed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,3 +1,2 @@
-//export const API_ENDPOINT = "http://localhost:5001/api";
 export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
-//https://full-stack-to-do-list-app-backend-server.onrender.com/api
+//https://template-external-deployable-full-stack.onrender.com/
